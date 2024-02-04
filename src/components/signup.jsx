1e84753fc5c491d99287bae433dc4705f3753cbf
nurcanNT <nrcntpkra@gmail.com>
2024-02-04 21:55:14 +0300

    fix: fixed image

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -107,11 +107,19 @@ const Signup = () => {
     <Grid container justifyContent="center" alignItems="center" sx={{mt:9}}>
       <Paper elevation={10} style={paperStyle}>
       <Grid container  justifyContent="center" alignItems="center" spacing={2} >
-          <Grid item xs={12} sm={6}>
-            <img src='/image/gif.gif'  alt="Colorful abstract painting"style={{ width: '100%',  height: 'auto', objectFit: 'cover', borderRadius: '230px', maxHeight: '70vh', }}  />
+          <Grid item xs={12} sm={6} >
+          <div style={{  height: '100vh', width: '100%', overflow: 'hidden' }}>
+            <img src='/image/gif.gif'  alt="Colorful abstract painting"style={{
+                    width: '100%',
+                    height: '100%',
+                    objectFit: 'cover',
+                    borderRadius: '230px',
+                    maxHeight: '70vh',
+                  }}  />
+                  </div>
           </Grid>
-          <Grid item xs={12} sm={6} style={{ ...gridStyle, flexDirection: 'column'  }}>
-          <Box sx={{ textAlign: 'center', mt: 1 }}>
+          <Grid item xs={12} sm={6} style={{ ...gridStyle, flexDirection: 'column',  overflow: 'hidden', height: '100vh', }}>
+          <Box sx={{ textAlign: 'center', mt: 1}}>
             <Typography variant="h3" sx={{mb: 2}}>Join us</Typography>
             <Typography variant="h5" color="gray">and start coding now</Typography>
             </Box>
@@ -168,10 +176,10 @@ const Signup = () => {
               }}
             />
           
-            <Button type='submit' color='primary' fullWidth variant="contained" sx={{ mt: 4, mb: 5, height: '60px' , fontSize: '15px'}}>
+            <Button type='submit' color='primary' fullWidth variant="contained" sx={{ mt: 1, mb: 4, height: '60px' , fontSize: '15px'}}>
               Create Account
             </Button>
-            <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', }}>
               <Typography variant="body1" color="gray">Already have an account?</Typography>
               <Box sx={{ ml: 2 }}>
                 <Link href="/login" underline="none">
