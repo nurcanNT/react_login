53ed559ea5317a6666c41db084de0a3b0a4cb7d4
nurcanNT <nrcntpkra@gmail.com>
2024-07-10 07:45:54 +0300

    feat: develop redux fixed

--- src/store.js
+++ src/store.js
@@ -1,6 +1,38 @@
-import { createStore } from 'redux';
-import rootReducer from './reducer/authReducer';
+import { createStore, combineReducers } from 'redux';
+import authReducer from './reducer/authReducer.js';
 
-const store = createStore(rootReducer);
+const rootReducer = combineReducers({
+  auth: authReducer,
+});
+
+const saveToLocalStorage = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+const loadFromLocalStorage = () => {
+  try {
+    const serializedState = localStorage.getItem('state');
+    if (serializedState === null) return undefined;
+    return JSON.parse(serializedState);
+  } catch (e) {
+    console.error(e);
+    return undefined;
+  }
+};
+
+const persistedState = loadFromLocalStorage();
+
+const store = createStore(
+  rootReducer,
+  persistedState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+store.subscribe(() => saveToLocalStorage(store.getState()));
 
 export default store;
